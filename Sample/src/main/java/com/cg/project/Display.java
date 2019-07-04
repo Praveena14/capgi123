96456cb2188ed9187081cba37d07a712fdffdd4e
pravpala <pravpala@DIN78000391.corp.capgemini.com>
2019-07-04 14:30:32 +0530

    commit

---
+++ Sample/src/main/java/com/cg/project/Display.java
@@ -0,0 +1,7 @@
+package com.cg.project;
+public class Display {
+	public void dis() {
+		
+		System.out.println("hiiii");
+	}
+}
---
+++ Sample/src/main/java/com/cg/project/Main.java
@@ -0,0 +1,9 @@
+package com.cg.project;
+
+public class Main {
+	public static void main(String args[]) {
+		Display d=new Display();
+		d.dis();
+	}
+
+}
